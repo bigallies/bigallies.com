891126ae3a16a6ca25029b95763816655fcbf4e7
NelsonDJCR <dev@nelsondjcr.com>
2024-11-19 10:36:48 -0500

    Update portfolio hover effect, refactor popup initialization, and add privacy policy page

--- js/main.js
+++ js/main.js
@@ -209,34 +209,11 @@
 });
     
     // Magnific Popup
-    $(".img-zoom").magnificPopup({
-    type: "image"
-    , closeOnContentClick: !0
-    , mainClass: "mfp-fade"
-    , gallery: {
-        enabled: !0
-        , navigateByImgClick: !0
-        , preload: [0, 1]
-    }
-});
-    $(document).ready(function() {
-        $('.popup-youtube, .popup-vimeo, .popup-gmaps, .popup-custom').magnificPopup({
-          disableOn: 700,
-          type: 'iframe',
-          mainClass: 'mfp-fade',
-          removalDelay: 160,
-          preloader: false,
-          fixedContentPos: false
-        });
 
-        $('.simple-ajax-popup-align-top').magnificPopup({
-          type: 'ajax',
-          alignTop: true,
-          overflowY: 'scroll' // as we know that popup content is tall we set scroll overflow by default to avoid jump
-        });
-        $('.simple-ajax-popup').magnificPopup({
-          type: 'ajax'
-        });
+    $(document).ready(function() {
+
+      
+
         
       });
     
